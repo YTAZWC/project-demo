3459398c7732d9ce34d1b672c06a2f79a668dfb0
花木凋零成兰 <18570354653@163.com>
2025-05-08 21:55:47 +0800

    分库分表实战-测试分片键_精确查询、范围查询

---
+++ src/test/java/top/ytazwc/ShardingTest.java
@@ -0,0 +1,52 @@
+package top.ytazwc;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import top.ytazwc.entity.Course;
+import top.ytazwc.mapper.CourseMapper;
+
+import java.util.List;
+
+/**
+ * @author 花木凋零成兰
+ * @title ShardingTest
+ * @date 2025-05-08 21:40
+ * @package top.ytazwc
+ * @description
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ShardingTest {
+
+    @Autowired
+    private CourseMapper courseMapper;
+
+    // 针对分片键进行精确查询 可以使用表达式控制
+    // SELECT cid,cname,user_id,cstatus FROM course WHERE (cid = ?)
+    @Test
+    public void queryCourse() {
+        LambdaQueryWrapper<Course> wrapper = new LambdaQueryWrapper<>();
+//        wrapper.eq(Course::getCid, 1127341091696148481L);
+        // in 当判断出查询的数据不在同一个分片时，放弃计算分片，进行全分片查询
+        wrapper.in(Course::getCid, 1127341091696148481L, 1127340884854046720L, 1127341092283351040L);
+        List<Course> courses = courseMapper.selectList(wrapper);
+        courses.forEach(System.out::println);
+    }
+
+    // 范围查询 测试分片效果
+    @Test
+    public void queryCourseRange() {
+        LambdaQueryWrapper<Course> wrapper = new LambdaQueryWrapper<>();
+        wrapper.between(Course::getCid, 1127341091926835200L, 1127341092253990912L);
+        // 依旧对全分片进行查询
+        List<Course> courses = courseMapper.selectList(wrapper);
+        courses.forEach(System.out::println);
+    }
+
+
+
+}
